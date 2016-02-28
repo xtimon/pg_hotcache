e0c7c85c7b392992fcdd9d3cc0fb7b4e75581cb5
Тимур Исанов <timur.isanov@bars-open.ru>
2016-02-28 21:47:18 +0300

    working script

--- pg_hotcache/core.py
+++ pg_hotcache/core.py
@@ -3,6 +3,7 @@ import psycopg2
 import psycopg2.extras
 import sys
 from argparse import ArgumentParser
+from re import compile
 
 
 def load_cache(args):
@@ -42,7 +43,7 @@ def load_cache(args):
         print(e)
     rows = cur.fetchall()
     version = rows[0][0].split()[1].split('.')
-    support_text = "Minimal supported PostgreSQL version is 9.4. Your version is {}.{}.{}".format(*version)
+    support_text = "Minimal supported PostgreSQL version is 9.4.\nYour PostgreSQL version is {}.{}.{}".format(*version)
     if version[0] == '9':
         if int(version[1]) < 4:
             print(support_text)
@@ -50,7 +51,60 @@ def load_cache(args):
     if int(version[0]) < 9:
         print(support_text)
         sys.exit(1)
-    
+    B = {
+        'kB': 1,
+        'MB': 2,
+        'GB': 3,
+        'TB': 4
+    }
+    pg_units = compile(r'([\d])+({}|{}|{}|{})'.format(*B.keys()))
+    try:
+        cur.execute(
+            """
+            SELECT setting, unit
+            FROM pg_settings
+            WHERE name='effective_cache_size'
+            """
+        )
+    except psycopg2.Error as e:
+        print(e)
+        sys.exit(1)
+    rows = cur.fetchall()
+    eff_cache_size_setting = int(rows[0][0])
+    eff_cache_size_unit = str(rows[0][1])
+    unit_opts = pg_units.findall(eff_cache_size_unit)
+    effective_cache_size = eff_cache_size_setting * int(unit_opts[0][0]) * 1024 ** B[unit_opts[0][1]]
+    try:
+        cur.execute(
+            """
+            SELECT relname, pg_relation_size(relid)
+            FROM pg_stat_all_tables
+            WHERE relname not like 'pg_%'
+            ORDER BY COALESCE(seq_scan, 0)+COALESCE(idx_scan, 0) DESC;
+            """
+        )
+    except psycopg2.Error as e:
+        print(e)
+        sys.exit(1)
+    rows = cur.fetchall()
+    tables = list()
+    i = 0
+    summary_size = 0
+    while summary_size < effective_cache_size:
+        summary_size += rows[i][1]
+        tables.append(rows[i][0])
+        i += 1
+    tables.pop()
+    for table in tables:
+        print("load table {} into cache".format(table))
+        try:
+            cur.execute(
+                """
+                select pg_prewarm('{}')
+                """.format(table)
+            )
+        except psycopg2.Error as e:
+            print(e)
 
 
 def main():
